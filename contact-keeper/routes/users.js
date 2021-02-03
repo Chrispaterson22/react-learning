38986c0f920885b352964416e42ee73bb431ee64
Chris Paterson <chrispaterson22@gmail.com>
2021-02-03 14:22:45 +0000

    added json web token, chnaged name of db to test

--- contact-keeper/routes/users.js
+++ contact-keeper/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../models/User')
@@ -41,7 +43,23 @@ router.post(
 
 			await user.save()
 
-			res.send('User saved')
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			}
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					expiresIn: 360000, // todo change back to 3600 for final
+				},
+				(err, token) => {
+					if (err) throw err
+					res.json({ token })
+				}
+			)
 		} catch (err) {
 			console.error(err.message)
 			res.status(500).send('Server Error')
