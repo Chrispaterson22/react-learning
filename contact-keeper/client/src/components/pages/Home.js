f1eed97a8e7e3931dcedb8417420e538bfe5d76d
Chris Paterson <chrispaterson22@gmail.com>
2021-02-04 17:05:25 +0000

    Contacts and contactItem components

--- contact-keeper/client/src/components/pages/Home.js
+++ contact-keeper/client/src/components/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Contacts from '../contacts/Contacts'
 
-const home = () => {
+const Home = () => {
 	return (
-		<div>
-			<h1>Home</h1>
+		<div className='grid-2'>
+			<div>{/* ContactForm */}</div>
+			<div>
+				<Contacts />
+			</div>
 		</div>
 	)
 }
 
-export default home
+export default Home
